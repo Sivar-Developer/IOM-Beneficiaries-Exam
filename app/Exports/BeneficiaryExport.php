3cbf2cc481ac61d4996124350c3fdd7c59002053
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-10-06 16:41:13 +0300

    Export excel with header and Import excel with defined header and ignore extra fields

--- app/Exports/BeneficiaryExport.php
+++ app/Exports/BeneficiaryExport.php
@@ -4,9 +4,29 @@ namespace App\Exports;
 
 use App\Beneficiary;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class BeneficiaryExport implements FromCollection
+class BeneficiaryExport implements FromCollection, WithHeadings
 {
+    public function headings(): array
+    {
+        return [
+            '#',
+            'Name',
+            'Birthdate',
+            'Gender',
+            'Phone Number',
+            'National ID Number',
+            'Mother Name',
+            'Martial Status',
+            'Employment Status',
+            'Monthly Income',
+            'Photo',
+            'Created At',
+            'Updated At'
+        ];
+    }
+
     /**
     * @return \Illuminate\Support\Collection
     */
--- app/Imports/BeneficiaryImport.php
+++ app/Imports/BeneficiaryImport.php
@@ -4,8 +4,9 @@ namespace App\Imports;
 
 use App\Beneficiary;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class BeneficiaryImport implements ToModel
+class BeneficiaryImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -15,16 +16,16 @@ class BeneficiaryImport implements ToModel
     public function model(array $row)
     {
         return new Beneficiary([
-            'name' => $row[1],
-            'birthdate' => $row[2],
-            'gender' => $row[3],
-            'phone_number' => $row[4],
-            'national_id_number' => $row[5],
-            'mother_name' => $row[6],
-            'martial_status' => $row[7],
-            'employment_status' => $row[8],
-            'monthly_income' => $row[9],
-            'photo' => $row[10],
+            'name' => $row['name'],
+            'birthdate' => $row['birthdate'],
+            'gender' => $row['gender'],
+            'phone_number' => $row['phone_number'],
+            'national_id_number' => $row['national_id_number'],
+            'mother_name' => $row['mother_name'],
+            'martial_status' => $row['martial_status'],
+            'employment_status' => $row['employment_status'],
+            'monthly_income' => $row['monthly_income'],
+            'photo' => $row['photo'],
         ]);
     }
 }
